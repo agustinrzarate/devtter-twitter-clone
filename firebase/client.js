c19002f371aac579e03c539c73fbdb1f50ee1477
agustin rafael zarate <agustinrzarate@gmail.com>
2022-03-22 09:19:08 -0300

    Create firebase config and login

---
+++ firebase/client.js
@@ -0,0 +1,37 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth, signInWithPopup, GithubAuthProvider, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3bXJFjGgqBPhqJoE_H_eaSwKNPzPdM1I',
+  authDomain: 'devtter-50d59.firebaseapp.com',
+  projectId: 'devtter-50d59',
+  storageBucket: 'devtter-50d59.appspot.com',
+  messagingSenderId: '354472592151',
+  appId: '1:354472592151:web:2e253c6c1e88d2f72b2d52',
+  measurementId: 'G-78PGE67WPX',
+};
+
+!getApps.length && initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const mapUserFromFirebaseAuth = (user) => {
+    const {displayName, photoURL, email } = user;
+    return {
+      email,
+      name: displayName,
+      avatar: photoURL,
+    };
+}
+
+export const loginWithGitHub = () => {
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider)
+};
+ 
+export const onAuthState = (onChange) => {
+    return onAuthStateChanged(auth, (user) => { 
+        const normalizeUser = mapUserFromFirebaseAuth(user);
+        onChange(normalizeUser);
+    });
+};
