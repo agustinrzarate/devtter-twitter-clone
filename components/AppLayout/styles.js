9b46aab343d9d13e2e7ba1161b19fc8faf3678ea
agustin rafael zarate <agustinrzarate@gmail.com>
2022-03-22 09:26:15 -0300

    added styles

--- components/AppLayout/styles.js
+++ components/AppLayout/styles.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
-import { fonts, colors } from '../../styles/theme';
+import { fonts, colors, breackpoints } from '../../styles/theme';
 import { addOpacityColor } from './../../styles/utils';
 
+
 const backgroundColor = addOpacityColor(colors.primary, 0.25);
+const shadow = addOpacityColor(colors.secondary, 0.15);
+
 export const App = styled.div`
   font-family: ${fonts.base};
-  background-image: radial-gradient(${backgroundColor} 1px, transparent 1px),
-    radial-gradient(${backgroundColor} 1px, transparent 1px);
+  background-image: radial-gradient(${backgroundColor} 1px, #fafafa 1px),
+    radial-gradient(${backgroundColor} 1px, #fafafa 1px);
   background-position: 0 0, 25px 25px;
   background-size: 50px 50px;
   padding: 0;
@@ -15,15 +18,20 @@ export const App = styled.div`
   overflow-y: hidden;
   display: grid;
   place-items: center;
-  h1 {
-    margin: 0;
-  }
+
   main {
-    height: 85vh;
+    width: 100%;
+    height: 100%;
     background-color: #fff;
     border-radius: 30px;
-    width: 450px;
-    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-    padding: 10px;
+    box-shadow: 0 10px 25px ${addOpacityColor(colors.secondary, 0.15)};
+    padding: 25px;
+  }
+
+  @media (min-width: ${breackpoints.mobile}) {
+    main {
+      height: 90vh;
+      width: ${breackpoints.mobile};
+    }
   }
 `;
